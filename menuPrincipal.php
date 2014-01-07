0986b5c4289616af107f86551e5655dfcbe5a640
cassio <cassio.ozarias@gmail.com>
2014-01-07 16:30:03 -0400

    [ADJUST] Comentando a verificacao de login
    Signed-off-by: cassio <cassio.ozarias@gmail.com>

--- menuPrincipal.php
+++ menuPrincipal.php
@@ -1,8 +1,8 @@
 <?php
-session_start();
-if (!$_SESSION['nome'] && !$_SESSION['cpf']) {
-    header("Location: index.php");
-}
+//session_start();
+//if (!$_SESSION['nome'] && !$_SESSION['cpf']) {
+//    header("Location: index.php");
+//}
 ?>
 
 <!DOCTYPE html>
