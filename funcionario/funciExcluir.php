d0073e84378ae7c3ff21c7bac1c26fd514d035ae
cassio <cassio.ozarias@gmail.com>
2014-01-07 18:09:28 -0400

    [ADJUST] função excluir usando PDO
    Signed-off-by: cassio <cassio.ozarias@gmail.com>

--- funcionario/funciExcluir.php
+++ funcionario/funciExcluir.php
@@ -2,5 +2,11 @@
 include '../conexao.php';
 $id = $_GET['id'];
 
-pg_query("delete from funcionario where id = $id;");
-header("Location: funciDados.php");
+try {
+    $stmt = $conn->prepare('DELETE FROM funcionario WHERE id =:id');
+    $stmt->bindParam(':id', $id);
+    $stmt->execute();
+    header("Location: funciDados.php");
+} catch (PDOException $e) {
+    echo 'Error:' . $e->getMessage();
+}
