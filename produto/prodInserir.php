23560d0e30e2d2d62662c1d3145f97ef3d22e1d1
cassio <cassio.ozarias@gmail.com>
2014-01-07 16:34:25 -0400

    [ADJUST] Inserindo produtos
    Signed-off-by: cassio <cassio.ozarias@gmail.com>

--- produto/prodInserir.php
+++ produto/prodInserir.php
@@ -7,46 +7,57 @@ $codigo = $_POST['codigo'];
 $categoria = $_POST['categoria'];
 
 if ($nome && $descricao) {
-    pg_query("insert into produto(nome,descricao,codigo,categoria)values('$nome','$descricao','$codigo','$categoria')");
-    header("location: prodDados.php");
+    try {
+        $stmt = $conn->prepare('INSERT INTO produto(nome,descricao,codigo,categoria)VALUES(:nome, :descricao, :codigo, :categoria)');
+        $stmt->execute(array(
+            ':nome' => $nome,
+            ':descricao' => $descricao,
+            ':codigo' => $codigo,
+            ':categoria' => $categoria,
+        ));
+        header("Location: prodDados.php");
+    } catch (Exception $e) {
+        echo 'Error:' . $e->getMessage();
+    }
 }
 
-$consulta = pg_query("SELECT * from categorias");
+$consulta  =$conn->prepare("SELECT * FROM categorias");
+$consulta ->execute();
 ?>
 <div class="col-md-6">
     <form class="form-horizontal" role="form" method="POST" name="frmcadastro">
         <fieldset>
             <legend>Dados de Produtos</legend>
             <div class="alert-info">
-                    <div class="form-group">
-                        <label for="inputnome2" class="col-sm-2 control-label">Nome:</label>
-                        <div class="col-sm-10">
-                            <input type="text" name="nome" class="col-lg-10" id="inputome2"  placeholder="Digite o nome do produto" required>
-                        </div>
-                    </div>                   
-                    <div class="form-group">
-                        <label for="inputdescricao3" class="col-sm-2 control-label">Descrição:</label>
-                        <div class="col-sm-10">
-                            <input type="text" name="descricao" class="col-lg-10" id="inputDescricao3" placeholder="Digite a discrição" required>
-                        </div>
+                <div class="form-group">
+                    <label for="inputnome2" class="col-sm-2 control-label">Nome:</label>
+                    <div class="col-sm-10">
+                        <input type="text" name="nome" class="col-lg-10" id="inputome2"  placeholder="Digite o nome do produto" required>
                     </div>
-                    <div class="form-group">
-                        <label for="inputcodigo3" class="col-sm-2 control-label">Código:</label>
-                        <div class="col-sm-10">
-                            <input type="text" name="codigo" class="col-lg-10" id="inputDescricao3" placeholder="Digite o código" required >
-                        </div>
-                    </div>
-                    <div class="form-group">
-                        <label for="inputcodigo3" class="col-sm-2 control-label">Categoria:</label>
-                        <div class="col-sm-10">
-                            <select name="categoria">
-                                <?php while ($linha = pg_fetch_object($consulta)): ?>
-                                    <option value="<?php echo $linha->id; ?>"><?php echo $linha->nome; ?></option>
-                                <?php endwhile; ?>
-                            </select>
-                        </div>
+                </div>                   
+                <div class="form-group">
+                    <label for="inputdescricao3" class="col-sm-2 control-label">Descrição:</label>
+                    <div class="col-sm-10">
+                        <input type="text" name="descricao" class="col-lg-10" id="inputDescricao3" placeholder="Digite a discrição" required>
                     </div>
                 </div>
+                <div class="form-group">
+                    <label for="inputcodigo3" class="col-sm-2 control-label">Código:</label>
+                    <div class="col-sm-10">
+                        <input type="text" name="codigo" class="col-lg-10" id="inputDescricao3" placeholder="Digite o código" required >
+                    </div>
+                </div>
+                <div class="form-group">
+                    <label for="inputcodigo3" class="col-sm-2 control-label">Categoria:</label>
+                    <div class="col-sm-10">
+                        <select name="categoria">
+                            <?php while ($linha = $consulta->fetch(PDO::FETCH_OBJ)): ?>
+                                <option value="<?php echo $linha->id; ?>"><?php echo $linha->nome; ?></option>
+                            <?php endwhile; ?>
+                        </select>
+                    </div>
+                </div>
+            </div>
             <input type="submit" class="btn btn-info" name="password" value="enviar" />
             <a href="prodDados.php"><input type="submit" class="btn btn-info" name="password" value="voltar"/></a>
         </fieldset>          
