4e877e14125fb77cc8114280f174153c18894a3a
cassio <cassio.ozarias@gmail.com>
2014-01-07 16:42:14 -0400

    [ADJUST] Query com inner join
    Signed-off-by: cassio <cassio.ozarias@gmail.com>

--- produto/prodDados.php
+++ produto/prodDados.php
@@ -2,33 +2,36 @@
 include '../menuPrincipal.php';
 include '../conexao.php';
 
-$rs =$conn->prepare("SELECT * FROM produto order by id");
+$rs = $conn->prepare("SELECT p.id, p.nome, p.descricao, p.codigo, c.nome as categoria
+from produto p
+inner join categorias c on (p.categoria = c.id)
+order by p.id;");
 $rs->execute();
 ?>
-    <div class="col-md-7 float- bottom- col-lg-offset-3 ">
-        <a href="prodInserir.php" class="btn btn-success">Novo Produto</a>
-        <table border="2" class="table table-hover" >
+<div class="col-md-7 float- bottom- col-lg-offset-3 ">
+    <a href="prodInserir.php" class="btn btn-success">Novo Produto</a>
+    <table border="2" class="table table-hover" >
+        <tr> 
+            <th>ID</th> 
+            <th>Nome</th>
+            <th>Descrição</th>
+            <th>Codigo</th>
+            <th>Categoria</th>
+            <th>Editar</th>
+            <th>Excluir</th>
+        </tr>
+        <?php while ($linha = $rs->fetch(PDO::FETCH_OBJ)): ?>
             <tr> 
-                <th>ID</th> 
-                <th>Nome</th>
-                <th>Descrição</th>
-                <th>Codigo</th>
-                <th>Categoria</th>
-                <th>Editar</th>
-                <th>Excluir</th>
+                <td><?php echo $linha->id; ?></td>
+                <td><?php echo $linha->nome; ?></td>
+                <td><?php echo $linha->descricao; ?></td>
+                <td><?php echo $linha->codigo; ?></td>
+                <td><?php echo $linha->categoria; ?></td>
+                <td><?php echo "<a href='prodEditar.php?id=$linha->id&nome=$linha->nome&descricao=$linha->descricao&codigo=$linha->codigo&categoria=$linha->categoria' class='btn btn-info'>Editar</a>"; ?></td>
+                <td><?php echo "<a href='prodExcluir.php?id=$linha->id' class='btn btn-danger'>Excluir</a>"; ?></td>
             </tr>
-            <?php while ($linha = $rs->fetch(PDO::FETCH_OBJ)): ?>
-                <tr> 
-                    <td><?php echo $linha->id; ?></td>
-                    <td><?php echo $linha->nome; ?></td>
-                    <td><?php echo $linha->descricao; ?></td>
-                    <td><?php echo $linha->codigo; ?></td>
-                    <td><?php echo $linha->categoria; ?></td>
-                    <td><?php echo "<a href='prodEditar.php?id=$linha->id&nome=$linha->nome&descricao=$linha->descricao&codigo=$linha->codigo&categoria=$linha->categoria' class='btn btn-info'>Editar</a>"; ?></td>
-                    <td><?php echo "<a href='prodExcluir.php?id=$linha->id' class='btn btn-danger'>Excluir</a>"; ?></td>
-                </tr>
-            <?php endwhile; ?>
-        </table>
-    </div>
+        <?php endwhile; ?>
+    </table>
+</div>
 </body>
 </html>
