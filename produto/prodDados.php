141c128d3c6e3ad5fe3fd55802eb305c12bafaad
cassio <cassio.ozarias@gmail.com>
2014-01-08 13:54:29 -0400

    [ADJUST] exibindo dados do produto usando PDO
    Signed-off-by: cassio <cassio.ozarias@gmail.com>

--- produto/prodDados.php
+++ produto/prodDados.php
@@ -2,7 +2,7 @@
 include '../menuPrincipal.php';
 include '../conexao.php';
 
-$rs = $conn->prepare("SELECT p.id, p.nome, p.descricao, p.codigo, c.nome as categoria
+$rs = $conn->prepare("SELECT p.id, p.nome, p.descricao, p.codigo, p.preco, c.nome as categoria
 from produto p
 inner join categorias c on (p.categoria = c.id)
 order by p.id;");
@@ -16,6 +16,7 @@ $rs->execute();
             <th>Nome</th>
             <th>Descrição</th>
             <th>Codigo</th>
+            <th>Preço</th>
             <th>Categoria</th>
             <th>Editar</th>
             <th>Excluir</th>
@@ -26,8 +27,9 @@ $rs->execute();
                 <td><?php echo $linha->nome; ?></td>
                 <td><?php echo $linha->descricao; ?></td>
                 <td><?php echo $linha->codigo; ?></td>
+                <td><?php echo $linha->preco; ?></td>
                 <td><?php echo $linha->categoria; ?></td>
-                <td><?php echo "<a href='prodEditar.php?id=$linha->id&nome=$linha->nome&descricao=$linha->descricao&codigo=$linha->codigo&categoria=$linha->categoria' class='btn btn-info'>Editar</a>"; ?></td>
+                <td><?php echo "<a href='prodEditar.php?id=$linha->id&nome=$linha->nome&descricao=$linha->descricao&codigo=$linha->codigo&preco=$linha->preco&categoria=$linha->categoria' class='btn btn-info'>Editar</a>"; ?></td>
                 <td><?php echo "<a href='prodExcluir.php?id=$linha->id' class='btn btn-danger'>Excluir</a>"; ?></td>
             </tr>
         <?php endwhile; ?>
