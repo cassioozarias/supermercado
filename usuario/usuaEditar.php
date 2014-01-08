ae7b256dd0465571e0c7c2fdb096e5aa64387b0c
cassio <cassio.ozarias@gmail.com>
2014-01-08 12:55:39 -0400

    [ADJUST] alterando usuario editar usando PDO
    Signed-off-by: cassio <cassio.ozarias@gmail.com>

--- usuario/usuaEditar.php
+++ usuario/usuaEditar.php
@@ -1,70 +1,50 @@
 <?php
 include '../menuPrincipal.php';
 include '../conexao.php';
-$id = $_GET['id'];
-$nome = $_POST['nome'];
-$descricao = $_POST['descricao'];
-$codigo = $_POST['codigo'];
-$categoria = $_POST['categoria'];
-$categoriaOrde = $_GET['categoria'];
+$id    = $_GET['id'];
+$login = $_POST['login'];
+$senha = $_POST['senha'];
+$loginOrde = $_GET['login'];
 
-if ($nome && $descricao) {
+if ($login&& $senha ) {
 
     try {
-        $stmt = $conn->prepare('UPDATE produto SET nome = :nome, descricao = :descricao, codigo = :codigo, categoria = :categoria WHERE id = :id');
+        $stmt = $conn->prepare('UPDATE usuario SET login = :login, senha = :senha WHERE id = :id');
         $stmt->execute(array(
-            ':id'        => $id,
-            ':nome'      => $nome,
-            ':descricao' => $descricao,
-            ':codigo'    => $codigo,
-            ':categoria' => $categoria,
+            ':id'     => $id,
+            ':login'  => $login,
+            ':senha'  => $senha,
         ));
-        header("Location: prodDados.php");
+        header("Location: usuaDados.php");
     } catch (Exception $e) {
         echo 'Error:' . $e->getMessage();
     }
 }
-$consulta = $conn->prepare("SELECT * FROM categorias order by nome = '$categoriaOrde' desc;");
+$consulta = $conn->prepare("SELECT * FROM usuario order by login = '$loginOrde' desc;");
 $consulta->execute();
 ?>
 <div class="col-md-6">
     <form class="form-horizontal" role="form" method="POST" name="frmcadastro">
         <fieldset>
-            <legend>Dados de Produtos</legend>
+            <legend>Dados do Usuário</legend>
             <div class="alert-info">
                 <div class="form-group">
-                    <label for="inputnome2" class="col-sm-2 control-label">Nome:</label>
+                    <label for="inputlogin2" class="col-sm-2 control-label">Login:</label>
                     <div class="col-sm-10">
-                        <input type="text" name="nome" class="col-lg-10" id="inputome2"  placeholder="Digite o nome do produto" required value="<?php echo $_GET['nome']; ?>">
+                        <input type="text" name="login" class="col-lg-10" id="inputlogin2"  placeholder="Digite seu login" required value="<?php echo $_GET['login']; ?>">
                     </div>
                 </div>                   
                 <div class="form-group">
-                    <label for="inputdescricao3" class="col-sm-2 control-label">Descrição:</label>
+                    <label for="inputsenha3" class="col-sm-2 control-label">Senha:</label>
                     <div class="col-sm-10">
-                        <input type="text" name="descricao" class="col-lg-10" id="inputDescricao3" placeholder="Digite a discrição" required value="<?php echo $_GET['descricao']; ?>">
-                    </div>
-                </div>
-                <div class="form-group">
-                    <label for="inputcodigo3" class="col-sm-2 control-label">Código:</label>
-                    <div class="col-sm-10">
-                        <input type="text" name="codigo" class="col-lg-10" id="inputDescricao3" placeholder="Digite o código" required value="<?php echo $_GET['codigo']; ?>">
-                    </div>
-                </div>
-                <div class="form-group">
-                    <label for="inputcodigo3" class="col-sm-2 control-label">Categoria:</label>
-                    <div class="col-sm-10">
-                        <select name="categoria">
-                            <?php while ($linha = $consulta->fetch(PDO::FETCH_OBJ)): ?>
-                                <option value="<?php echo $linha->id; ?>"><?php echo $linha->nome; ?></option>
-                            <?php endwhile; ?>
-                        </select>
+                        <input type="text" name="senha" class="col-lg-10" id="inputSenha3" placeholder="Digite sua senha" required value="<?php echo $_GET['senha']; ?>">
                     </div>
                 </div>
             </div>
             <input type="submit" class="btn btn-info" name="password" value="enviar" />
-            <a href="prodDados.php"><input type="submit" class="btn btn-info" name="password" value="voltar"/></a>
-        </fieldset>          
-    </form> 
-</div>  
+            <a href="usuaDados.php"><input type="submit" class="btn btn-info" name="password" value="voltar"/></a>
+        </fieldset>
+    </form>
+</div>
 </body>
 </html>
