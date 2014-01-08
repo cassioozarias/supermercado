65d5d8b6ddca5ce08bfcf04140d4173075e587bc
cassio <cassio.ozarias@gmail.com>
2014-01-08 12:57:39 -0400

    [ADJUST] alterando usuario excluir usando PDO
    Signed-off-by: cassio <cassio.ozarias@gmail.com>

--- usuario/usuaExcluir.php
+++ usuario/usuaExcluir.php
@@ -4,10 +4,10 @@ include '../conexao.php';
 $id = $_GET['id'];
 
 try {
-    $stmt = $conn->prepare('DELETE FROM produto WHERE id =:id');
+    $stmt = $conn->prepare('DELETE FROM usuario WHERE id =:id');
     $stmt->bindParam(':id', $id);
     $stmt->execute();
-    header("Location: prodDados.php");
+    header("Location: usuaDados.php");
 } catch (PDOException $e) {
     echo 'Error:' . $e->getMessage();
 }
