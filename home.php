7f5fc9fe1f850fd95d239ce89796d992ad1d7c76
cassio <cassio.ozarias@gmail.com>
2014-03-13 16:00:12 -0400

    [ADD] Banco
    Signed-off-by: cassio <cassio.ozarias@gmail.com>

--- home.php
+++ home.php
@@ -1,6 +1,6 @@
 <?php include './menuPrincipal.php';?>
         <div class="navbar-left navbar float- bottom- col-lg-offset-4">            
-            <h1 class="alert-info">Bem-vindo ao Supermercasoft Perfect!</h1>        
+            <h1 class="alert-info">Bem-vindo ao Mercasoft Perfect!</h1>        
         </div>
     </body>
 </html>
